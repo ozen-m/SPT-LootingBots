1facb5526b8d5ea8fa7075ad805b04b08118b718
ozen-m <74519617+ozen-m@users.noreply.github.com>
2026-03-15 16:46:36 +0800

    Inline assignment

--- LootingBots/Components/ItemAppraiser.cs
+++ LootingBots/Components/ItemAppraiser.cs
@@ -52,14 +52,12 @@ public class ItemAppraiser(Log _log)
                 var completionClass = new TaskCompletionSource<Result<Dictionary<string, float>>>();
                 Singleton<ClientApplication<ISession>>.Instance.GetClientBackEndSession().RagfairGetPrices(completionClass.SetResult);
 
-                Dictionary<MongoID, float> prices = null;
                 var ragfairPrices = await completionClass.Task;
                 if (ragfairPrices.Succeed)
                 {
-                    prices = ragfairPrices.Value.ToDictionary(pair => new MongoID(pair.Key), pair => pair.Value);
+                    MarketData = ragfairPrices.Value.ToDictionary(pair => new MongoID(pair.Key), pair => pair.Value);
                 }
 
-                MarketData = prices;
                 if (MarketData is null)
                 {
                     _log.LogInfo("Failed to get flea prices from BE session");
