f4f1ad864bd120ce210db43b52264f11e9db1a22
ozen-m <74519617+ozen-m@users.noreply.github.com>
2026-09-01 13:38:51 +0800

    Support cancellation externally, add docs

--- LootingBots/Utilities/TimeoutController.cs
+++ LootingBots/Utilities/TimeoutController.cs
@@ -9,14 +9,27 @@ using UnityEngine;
 
 namespace LootingBots.Utilities;
 
+/// <summary>
+/// This controller manages the creation and cancellation of a <see cref="CancellationTokenSource"/>.
+/// It reuses the <see cref="CancellationTokenSource"/> until a timeout occurs, a new source is then created when asked for a new timeout.
+/// </summary>
 public sealed class TimeoutController : MonoBehaviour
 {
     private CancellationTokenSource _timeoutSource;
     private float _remainingTime;
     private bool _isRunning;
+    private bool _isCanceledExternally;
 
-    public bool IsTimeout => _timeoutSource.IsCancellationRequested;
+    public bool IsActive => _isRunning;
+    public bool IsCanceled => _timeoutSource.IsCancellationRequested;
+    public bool IsTimeout => _timeoutSource.IsCancellationRequested && !_isCanceledExternally;
 
+    /// <summary>
+    /// Starts a timeout and returns a CancellationToken
+    /// </summary>
+    /// <param name="secondsTimeout">The timeout duration, in seconds</param>
+    /// <returns>A <see cref="CancellationToken"/> that is canceled when the timeout expires.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if a timeout is already running.</exception>
     public CancellationToken Timeout(float secondsTimeout)
     {
         if (_isRunning)
@@ -31,12 +44,26 @@ public sealed class TimeoutController : MonoBehaviour
 
         _remainingTime = secondsTimeout;
         _isRunning = true;
+        _isCanceledExternally = false;
         return _timeoutSource.Token;
     }
 
+    /// <summary>
+    /// Stops the timeout without canceling its cancellation token.
+    /// </summary>
     public void ResetTimer()
     {
         _isRunning = false;
+    }
+
+    /// <summary>
+    /// Manually cancels the currently running timeout.
+    /// This cancellation is distinguished from cancellation caused by the timeout elapsing.
+    /// </summary>
+    public void Cancel()
+    {
+        _isCanceledExternally = true;
+        SignalCancellation();
     }
 
     private void Awake()
@@ -67,7 +94,7 @@ public sealed class TimeoutController : MonoBehaviour
 
     private void OnDestroy()
     {
-        _timeoutSource.Cancel();
+        Cancel();
         _timeoutSource.Dispose();
     }
 }
