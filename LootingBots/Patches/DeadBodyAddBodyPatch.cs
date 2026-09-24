fb34aa8b0fe1c97703094ad6236dab65a0224210
ozen-m <74519617+ozen-m@users.noreply.github.com>
2026-09-24 11:10:56 +0800

    Disable BotDeadBodyWork

---
+++ LootingBots/Patches/DeadBodyAddBodyPatch.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+using EFT;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+
+namespace LootingBots.Patches;
+
+/// <summary>
+/// Runs when a player is removed as an ally/enemy in <see cref="BotsGroup"/>. <see cref="BotDeadBodyWork"/> does not run with LB.
+/// </summary>
+public class DeadBodyAddBodyPatch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(DeadBodiesController).GetMethod(nameof(DeadBodiesController.AddBody), [typeof(IPlayer)]);
+    }
+
+    [PatchTranspiler]
+    public static IEnumerable<CodeInstruction> Transpile()
+    {
+        return [new CodeInstruction(OpCodes.Ret)];
+    }
+}
---
+++ LootingBots/Patches/DeadBodyUpdatePatch.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+
+namespace LootingBots.Patches;
+
+/// <summary>
+/// Runs in layers <see cref="PatrolActionsLayer"/> and <see cref="FollowerPatrolLayer"/>.
+/// Won't run in Utility peace anymore since layer is removed, but still runs in PatrolFollower.
+/// </summary>
+public class DeadBodyUpdatePatch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(BotDeadBodyWork).GetMethod(nameof(BotDeadBodyWork.UpdateCheck));
+    }
+
+    [PatchTranspiler]
+    public static IEnumerable<CodeInstruction> Transpile()
+    {
+        return [new CodeInstruction(OpCodes.Ret)];
+    }
+}
